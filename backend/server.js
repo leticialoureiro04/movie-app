9399c91dda899dcbd54163d3e4ef391df3c8886d
Letícia Loureiro <leticialoureiro@ipvc.pt>
2025-05-07 19:52:47 +0100

    Modificações 2

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://movie-app-frontend-bmrg.onrender.com' 
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
